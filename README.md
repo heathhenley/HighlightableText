2a6b9651950684566967ffc3c38cc95f5811a2ee
Heath Henley <heath.j.henley@gmail.com>
2023-10-15 12:29:40 -0400

    Updates: almost working with this approach

--- README.md
+++ README.md
@@ -2,5 +2,9 @@ Trying to make a component that takes text and renders
 it, but allows selections from the user to be highlighed.
 I found: https://medium.com/unprogrammer/a-simple-text-highlighting-component-with-react-e9f7a3c1791a
 
-Which is what I got this started based on. However, I want
-the use to be able to highlight multiple sections of text.
+Based on the approach there, I'm basically trying to generalize it.
+
+It still has a lot of problems:
+- select text that spans multiple spans
+- sometimes it doesn't seem to highlight
+- not working when last character selected is one of the ends of the whole text
--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,101 @@
 import { useState } from "react";
 import "./App.css";
 
-const dummyText =
-  "This is a bunch of text that we want to render, in the real system this comes from Convex. This is a bunch of text that we want to render, in the real system this comes from Convex. This is a bunch of text that we want to render, in the real system this comes from Convex. This is a bunch of text that we want to render, in the real system this comes from Convex.";
+const dummyText = "This is a bunch of dummy text to test highlighting! This is a bunch of dummy text to test highlighting.";
 
-type TWord = {
+type TTextSection = {
   text: string;
   highlighted: boolean;
 };
+
+// respesents the text as a series of spans, each of which is either
+// highlighted or not, we start out with just one span that is not highlighted
+type TOrderedTextSection = TTextSection[];
+
 const highlightedStyle = {
   backgroundColor: "yellow",
   cursor: "pointer",
 };
 
-const HighlightableText = ({text}: {text: string}) => {
-  const [highlighted, setHighlighted] = useState<TWord[]>(
-    [
-      { text: text, highlighted: false },
-    ]
-  );
+const HighlightableText = ({ text }: { text: string }) => {
+  const [highlighted, setHighlighted] = useState<TOrderedTextSection>([
+    { text: text, highlighted: false },
+  ]);
 
   const handleHighlightText = (e: React.MouseEvent) => {
     e.preventDefault();
     const selection = window.getSelection();
-    if (selection) {
-      console.log(selection);
-      let start = selection.anchorOffset;
-      let end = selection.focusOffset;
-      // this is if the user selects from right to left
-      if (start > end) {
-        [start, end] = [end, start];
-      }
-      const highlightedText = selection.toString();
-      // correct offsets for parent elements
-      const parent = selection.anchorNode?.parentElement;
-      if (parent?.getAttribute("data-order") === "middle") {
-        console.log("middle");
-        // we need the start of the select in the actual text, but
-        // we have them in separate spans to highlight, so we
-        // need to add the length of the first span (not highlighted)
-        // to the start and end offsets
-        start += highlighted[0].text.length;
-        end += highlighted[0].text.length;
-      } else if (parent?.getAttribute("data-order") === "last") {
-        console.log("last");
-        // same as above but need both the first and second spans
-        start += highlighted[0].text.length + highlighted[1].text.length;
-        end += highlighted[0].text.length + highlighted[1].text.length;
-      }
-      setHighlighted([
-        { text: text.slice(0, start), highlighted: false },
-        { text: highlightedText, highlighted: true },
-        { text: text.slice(end), highlighted: false },
-      ]);
+    if (!selection) {
+      return;
     }
+    // These are relative to the span, not the whole text
+    let start = selection.anchorOffset;
+    let end = selection.focusOffset;
+    // swap if the user selects from right to left
+    if (start > end) {
+      [start, end] = [end, start];
+    }
+
+    const tmpHighlighted: TOrderedTextSection = [];
+
+    // compensate the start/end offsets for the lengths of the previous spans
+    let endOfLastSpanBeforeSelection = 0;
+    let endOfSpanContainingSelection = 0;
+    const grandpaNode = selection.anchorNode?.parentNode?.parentNode;
+    grandpaNode?.childNodes.forEach((child, idx) => {
+      // is this child before the selection?
+      let position = selection.anchorNode?.compareDocumentPosition(child);
+      if (position && position === Node.DOCUMENT_POSITION_PRECEDING) {
+        start += child.textContent?.length || 0;
+        end += child.textContent?.length || 0;
+        endOfLastSpanBeforeSelection += child.textContent?.length || 0;
+        tmpHighlighted.push({
+          text: child.textContent || "",
+          highlighted: highlighted[idx].highlighted,
+        });
+      }
+      if (position && position === 10) {
+        endOfSpanContainingSelection = child.textContent?.length || 0;
+        // this is the existing span containing our selection - we need to add
+        // the beginning of it before the selection as unhighlighted
+        tmpHighlighted.push({
+          text: text.slice(endOfLastSpanBeforeSelection, start) || "",
+          highlighted: highlighted[idx].highlighted,
+        });
+      }
+    });
+    // add the current selection as highlighted, and rest of the text in the
+    // span containing the selection as unhighlighted
+    const endOfSpanContainingSelectionInText =
+      endOfLastSpanBeforeSelection + endOfSpanContainingSelection;
+    tmpHighlighted.push({ text: text.slice(start, end), highlighted: true });
+    tmpHighlighted.push({
+      text: text.slice(end, endOfSpanContainingSelectionInText),
+      highlighted: false,
+    });
+
+    // add the rest of the spans back in
+    grandpaNode?.childNodes.forEach((child, idx) => {
+      // is this child before the selection?
+      let position = selection.anchorNode?.compareDocumentPosition(child);
+      if (position && position === Node.DOCUMENT_POSITION_FOLLOWING) {
+        console.log(idx, child);
+        tmpHighlighted.push({
+          text: child.textContent || "",
+          highlighted: highlighted[idx].highlighted,
+        });
+      }
+    });
+    setHighlighted(tmpHighlighted);
   };
+
   return (
     <div onMouseUp={(e) => handleHighlightText(e)}>
-        <span data-order="first">{highlighted[0].text}</span>
-        <span style={highlightedStyle} data-order="middle">{highlighted[1]?.text}</span>
-        <span data-order="last">{highlighted[2]?.text}</span>
+      {highlighted.map((word, idx) => (
+        <span key={idx} style={word.highlighted ? highlightedStyle : {}}>
+          {word.text}
+        </span>
+      ))}
     </div>
   );
 };
